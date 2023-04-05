2c0d6073335857fa04478f6cf40fb4b4f34c7fb2
MercyChebet <chebetm96@gmail.com>
2023-04-05 18:46:57 +0300

    Task 0

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,29 +1,35 @@
 #!/usr/bin/python3
-
-"""
-    Module 0 task which determines the number of minmum operations given n characters
+"""Task 0 task
 """
 
 
 def minOperations(n):
-    """
-        The function calculates the fewest number of operations
+    """The function alculates the fewest number of operations
         needed to give a result of exactly n H characters in a file
         args: n: Number of characters to be displayed
         return:
                number of min operations
+    Args:
+        n (int): The number of desired n characters.
+    Returns:
+        int: The number of minimal operations needed to get n characters
+    or 0 if it is impossible to achieve n.
     """
-
-    now = 1
-    start = 0
-    counter = 0
-    while now < n:
-        remainder = n - now
-        if (remainder % now == 0):
-            start = now
-            now += start
-            counter += 2
-        else:
-            now += start
-            counter += 1
-    return counter
+    if not isinstance(n, int):
+        return 0
+    ops_count = 0
+    clipboard = 0
+    done = 1
+    while done < n:
+        if clipboard == 0:
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+        elif n - done > 0 and (n - done) % done == 0:
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+        elif clipboard > 0:
+            done += clipboard
+            ops_count += 1
+    return ops_count
