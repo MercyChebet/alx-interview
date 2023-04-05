1b90c54bcd908c7a4948ac670e6684c04ef21436
MercyChebet <chebetm96@gmail.com>
2023-04-05 18:53:45 +0300

    Task 0

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,32 +1,27 @@
+
 #!/usr/bin/python3
-"""Task 0 task
+""" Script that computes a minimum operations
+    needed in a CopyAll - Paste task
 """
 
 
-
-    """The function alculates the fewest number of operations
-        needed to give a result of exactly n H characters in a file
-        args: n: Number of characters to be displayed
-        return:
-               number of min operations
-    Args:
-        n (int): The number of desired n characters.
-    Returns:
-        int: The number of minimal operations needed to get ncharacters
-    or 0 if it is impossible to achieve n.
+def minOperations(n):
     """
-
-
-    now = 1
-    start = 0
-    counter = 0
-    while now < n:
-        remainder = n - now
-        if (remainder % now == 0):
-            start = now
-            now += start
-            counter += 2
-        else:
-            now += start
-            counter += 1
-    return counter
+    Method for compute the minimum number
+    of operations for task Copy All and Paste
+    Args:
+        n: input value
+        factor_list: List to save the operations
+    Return: the sum of the operations
+    """
+    if n < 2:
+        return 0
+    factor_list = []
+    i = 1
+    while n != 1:
+        i += 1
+        if n % i == 0:
+            while n % i == 0:
+                n /= i
+                factor_list.append(i)
+    return sum(factor_list)
