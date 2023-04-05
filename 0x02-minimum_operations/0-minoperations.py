80b9bb10b2345e7e99c80ec865556c4d7a6c9df5
MercyChebet <chebetm96@gmail.com>
2023-04-05 18:49:52 +0300

    Task 0

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,7 +3,7 @@
 """
 
 
-def minOperations(n):
+
     """The function alculates the fewest number of operations
         needed to give a result of exactly n H characters in a file
         args: n: Number of characters to be displayed
@@ -15,20 +15,18 @@ def minOperations(n):
         int: The number of minimal operations needed to get ncharacters
     or 0 if it is impossible to achieve n.
     """
-    idef minOperations(n):
-    """
-    Single character H
-    Fewest number of operations
-    """
 
-    if n <= 1:
-        return 0
-    numbr, index, operations = n, 2, 0
 
-    while numbr > 1:
-        if numbr % index == 0:
-            numbr = numbr / index
-            operations = operations + index
+    now = 1
+    start = 0
+    counter = 0
+    while now < n:
+        remainder = n - now
+        if (remainder % now == 0):
+            start = now
+            now += start
+            counter += 2
         else:
-            index += 1
-    return operations
+            now += start
+            counter += 1
+    return counter
