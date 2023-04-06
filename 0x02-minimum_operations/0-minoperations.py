9a91d38a89d8d02d760c0d426728a8d3c1880dea
MercyChebet <chebetm96@gmail.com>
2023-04-07 00:20:47 +0300

    Task 0

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python3
-
-'''
-The funtion returns minimum number of operation to carry out
-copy all and paste operations
-'''
+""" Minimum Operations
+    """
 
 
-def minOperations(n):
-    '''
-    The function takes the least common factor of the given number
-    '''
-    if (n < 2):
-        return 0
-    factors = []
-    divisor = 2
-    while (n >= 2):
-        # check if n is divisible by 2
-        if (n % divisor == 0):
-            factors.append(divisor)
-            n /= divisor
+def minOperations(n: int) -> int:
+    """ Minimum Operations needed to get n H characters """
+    next = 'H'
+    body = 'H'
+    op = 0
+    while (len(body) < n):
+        if n % len(body) == 0:
+            op += 2
+            next = body
+            body += body
         else:
-            divisor += 1
-    return sum(factors)
+            op += 1
+            body += next
+    if len(body) != n:
+        return 0
+    return op
