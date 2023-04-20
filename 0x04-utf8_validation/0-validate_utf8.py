3068c8984da8be7731c95c195e953165bbc88f04
MercyChebet <chebetm96@gmail.com>
2023-04-21 02:11:34 +0300

    Task 0

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,20 +1,39 @@
 #!/usr/bin/python3
-"""Module for validUtf8 method"""
+"""UTF-8 Validation"""
 
 
 def validUTF8(data):
-    """Validates UTF 8"""
-    b = 0
-    for i, n in enumerate(data):
-        byte = n & 0xFF
-        if b:
-            if byte >> 6 != 2:
+    """The function determines if a given data set
+    represents a valid utf-8 encoding
+    """
+    number_bytes = 0
+
+    mask_1 = 1 << 7
+    mask_2 = 1 << 6
+
+    for i in data:
+
+        mask_byte = 1 << 7
+
+        if number_bytes == 0:
+
+            while mask_byte & i:
+                number_bytes += 1
+                mask_byte = mask_byte >> 1
+
+            if number_bytes == 0:
+                continue
+
+            if number_bytes == 1 or number_bytes > 4:
                 return False
-            b -= 1
-            continue
-        while (1 << abs(7 - b)) & byte:
-            b += 1
-        if b == 1 or b > 4:
-            return False
-        b = max(b - 1, 0)
-    return b == 0
+
+        else:
+            if not (i & mask_1 and not (i & mask_2)):
+                return False
+
+        number_bytes -= 1
+
+    if number_bytes == 0:
+        return True
+
+    return False
