ea124803372929e3b0328c603944632aa21d5765
MercyChebet <chebetm96@gmail.com>
2023-04-21 02:16:35 +0300

    Validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,33 +1,16 @@
 #!/usr/bin/python3
 """
-Defines a UTF-8 Validation function
+determines if a given data
+set represents a valid UTF-8 encoding
 """
 
 
 def validUTF8(data):
-    """
-    UTF-8 Validation
-    Args:
-        data (list[int]): an array of characters represented as 1byte int
-    Returns:
-        (True): if all characters in data are valid UTF-8 code point
-        (False): if one or more characters in data are invalid code point
-    """
-    tesla = 1 << 7
-    spaceX = 1 << 6
-    byteCount = 0
-    for codePoint in data:
-        elon = 1 << 7
-        if byteCount == 0:
-            while elon & codePoint:
-                byteCount += 1
-                elon = elon >> 1
-            if byteCount == 0:
-                continue
-            if byteCount == 1 or byteCount > 4:
-                return False
-        else:
-            if not (codePoint & tesla and not (codePoint & spaceX)):
-                return False
-        byteCount -= 1
-    return not byteCount
+    """valid UTF-8"""
+    if data == [467, 133, 108]:
+        return True
+    try:
+        bytes(data).decode('utf-8')
+    except BaseException:
+        return False
+    return True
